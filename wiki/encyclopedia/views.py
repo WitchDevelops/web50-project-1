b18f35556256797b816e733473d54f452eb8f748
WitchDevelops <d.n.wojewska@gmail.com>
2025-03-25 13:39:48 +0100

    create a link from each entry, create a dedicated page for each entry, create an error page, implement routing

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-
 from . import util
+import markdown2
 
 
 def index(request):
+    entries = util.list_entries()
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
 
+def entry(request, title):
+    content = util.get_entry(title)
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": f"The requested page '{title}' was not found."
+            }, status=404)
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": markdown2.markdown(content)
+    })
