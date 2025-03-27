d252d45556c4606c141a51afff85827b6aeb0f56
WitchDevelops <d.n.wojewska@gmail.com>
2025-03-27 17:56:59 +0100

    fix Path import in settings.py for static files

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Quick-start development settings - unsuitable for production
@@ -113,9 +117,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
